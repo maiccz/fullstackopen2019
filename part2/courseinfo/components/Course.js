eef389213d944339a137f0294506f023b0035bd9
maiccz <45683629+maiccz@users.noreply.github.com>
2019-12-23 08:35:17 +0100

    2.1: course contents step6

---
+++ part2/courseinfo/components/Course.js
@@ -0,0 +1,35 @@
+import React from 'react'
+C
+const Header = ({ course }) => {
+  return (
+      <h1>{course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+          {props.part} {props.exercises}
+      </p>
+  )
+}
+
+const Content = ({ course }) => {
+
+  return (
+      <div>
+        {course.parts.map(part => <Part part={part.name} exercises={part.exercises}/>)}
+      </div>
+    )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+export default Course
