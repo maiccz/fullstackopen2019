d5111b1ee5aed12cda4950ed323422e06b9bc75a
maiccz <45683629+maiccz@users.noreply.github.com>
2019-12-28 15:09:52 +0100

    Add files via upload

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,99 +1,103 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const cors = require('cors')
 
-morgan.token('post-body', function getPostBody (request) {
-  if (request.method === 'POST') {
-    return JSON.stringify(request.body)
-  }
-})
-
+app.use(cors())
 app.use(bodyParser.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
   },
-  { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
   },
-  { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-  },
-  { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
   }
 ]
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
 const generateId = () => {
-  return Math.floor(Math.random() * 100000) + 100000
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
 }
 
-app.get('/info', (request, response) => {
-  const date = new Date()
-  response.send(`
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${date}`)
-})
-
-app.post('/api/persons', (request, response) => {
+app.post('/notes', (request, response) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  } else if (persons.some(p => p.name === body.name)){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
+  const note = {
+    content: body.content,
+    important: body.important || false,
     date: new Date(),
-    id: generateId()
+    id: generateId(),
   }
 
-  persons = persons.concat(person)
+  notes = notes.concat(note)
 
-  response.json(person)
+  response.json(note)
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/notes', (request, response) => {
+  response.json(notes)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
 })
 
-const PORT = 3002
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
