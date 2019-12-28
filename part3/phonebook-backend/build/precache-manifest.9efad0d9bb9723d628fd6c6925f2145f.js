8ec67754e777d42338eb031cba2580b4249f765a
maiccz <45683629+maiccz@users.noreply.github.com>
2019-12-28 20:57:29 +0100

    3.9-11 phonebook backend step9-11
    https://mighty-anchorage-01871.herokuapp.com/

---
+++ part3/phonebook-backend/build/precache-manifest.9efad0d9bb9723d628fd6c6925f2145f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57809e9f446d53684a3571f351f57e66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d450c20cb69a2d5354",
+    "url": "/static/css/main.feecf882.chunk.css"
+  },
+  {
+    "revision": "6e5a5b07b3d911c16341",
+    "url": "/static/js/2.718641af.chunk.js"
+  },
+  {
+    "revision": "0720a2c09447309283199a78e1b05bef",
+    "url": "/static/js/2.718641af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8d450c20cb69a2d5354",
+    "url": "/static/js/main.85eb7538.chunk.js"
+  },
+  {
+    "revision": "9cc21be3af7473167c4d",
+    "url": "/static/js/runtime-main.4fe03d3f.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,103 +1,104 @@
 const express = require('express')
 const app = express()
-
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(cors())
+morgan.token('post-body', function getPostBody (request) {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+})
+
 app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
+app.use(cors())
+app.use(express.static('build'))
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
   },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
   },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+  return Math.floor(Math.random() * 100000) + 100000
 }
 
-app.post('/notes', (request, response) => {
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}`)
+})
+
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  } else if (persons.some(p => p.name === body.name)){
+    return response.status(400).json({ 
+      error: 'name must be unique' 
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  const person = {
+    name: body.name,
+    number: body.number,
     date: new Date(),
-    id: generateId(),
+    id: generateId()
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
-app.get('/notes', (request, response) => {
-  response.json(notes)
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-app.delete('/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
