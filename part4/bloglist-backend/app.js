b857b3341f05a2d9b8eb2d25c86fa36fd28a367e
maiccz <45683629+maiccz@users.noreply.github.com>
2019-12-29 18:55:29 +0100

    4.1-2 Blog list, step1-2

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
