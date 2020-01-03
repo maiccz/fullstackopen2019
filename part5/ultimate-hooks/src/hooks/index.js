ad755a822bd536add67021c56554cb7d8ed7c3c0
maiccz <45683629+maiccz@users.noreply.github.com>
2020-01-03 22:26:30 +0100

    5.21*: ultimate hooks

---
+++ part5/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  const getAll = () => {
+    const request = axios.get(baseUrl)
+    request.then(response => setResources(response.data))
+  }
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    getAll, create
+  }
+
+  return [
+    resources, service
+  ]
+}
