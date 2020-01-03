21b3f4ee459cd75dcf0d72b08fff8c2898622b7f
maiccz <45683629+maiccz@users.noreply.github.com>
2020-01-03 11:17:35 +0100

    5.13-15: Blog list tests, step1-3

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -15,16 +15,13 @@ const Blog = ({ blog, incrementLikesOf, removeBlog }) => {
     ? true
     : false
 
-    console.log(blog.user.username, typeof(blog.user.username))
-    console.log(localStorage.getItem('loggedBlogappUser').username, typeof(localStorage.getItem('loggedBlogappUser').username))
-
   return (
     <div>
-      <div className='blog blog-title' style={hideWhenVisible} onClick={() => setLoginVisible(true)}>
+      <div className='blog blog-title' data-testid="visiblebyDefault" style={hideWhenVisible} onClick={() => setLoginVisible(true)}>
         {blog.title} {blog.author}
       </div>
-      <div className='blog' style={showWhenVisible} >
-          <div className='blog-title'onClick={() => setLoginVisible(false)}>
+      <div className='blog hiddenByDefault'style={showWhenVisible} >
+          <div className='blog-title' onClick={() => setLoginVisible(false)}>
             {blog.title} {blog.author}</div>
           <a href={blog.url}>{blog.url}</a><br />
           {blog.likes} likes <button onClick={() => incrementLikesOf(blog.id)}>like</button><br />
---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import Blog from './Blog'
+
+const blog = {
+  title: 'Type wars',
+  author: 'Robert C. Martin',
+  likes: 2,
+  url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+  user: {username: 'Test'}
+}
+
+localStorage.setItem('loggedBlogappUser', JSON.stringify(blog.user))
+
+let component
+
+beforeEach(() => {
+  component = render(
+    <Blog blog={blog}/>
+  )
+})
+
+test('renders component with correct content', () => {
+  expect(component.container).toHaveTextContent(
+    `${blog.title}`
+)
+
+  expect(component.container).toHaveTextContent(
+    `${blog.author}`
+  )
+
+  expect(component.container).toHaveTextContent(
+    `${blog.likes} likes`
+  )
+  expect(component.container).toHaveTextContent(
+    `${blog.url}`
+  )
+
+})
+
+test('at start the children are not displayed', () => {
+  const div = component.container.querySelector('.hiddenByDefault')
+
+  expect(div).toHaveStyle('display: none')
+})
+
+test('after clicking the title, children are displayed', () => {
+  const title = component.getByTestId('visiblebyDefault')
+  fireEvent.click(title)
+
+  const div = component.container.querySelector('.hiddenByDefault')
+  expect(div).not.toHaveStyle('display: none')
+})
