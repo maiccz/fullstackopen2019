40d481bd638622b2286c341a28f5cbd73e413eab
maiccz <45683629+maiccz@users.noreply.github.com>
2020-01-04 20:04:54 +0100

    6.3-8: anecdotes, step1-6

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createAnegdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = (props) => {
+  const addAnegdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anegdote.value
+    event.target.anegdote.value = ''
+    props.store.dispatch(
+      createAnegdote(content)
+    )
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnegdote}>
+        <input name="anegdote" />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
